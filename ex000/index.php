2e88f5aad944bc6e54d828ffc64985fb5d000a7c
JorderGomes <jordernoveau@gmail.com>
2026-03-03 17:56:22 -0300

    exemplo 0

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Me livrando da maldição! \u{1F47B}";
+        ?>
+    </h1>
+    <p>Estou tentando me livrar da maldição.</p>
+</body>
+</html>
