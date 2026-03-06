ffbeeec4090ca78ce374c302c0506265270a16d9
JorderGomes <jordernoveau@gmail.com>
2026-03-06 10:50:32 -0300

    exemplo 10

---
+++ ex010/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Número aleatório</title>
+</head>
+<body>
+    <header>
+        <h1>Número aleatório</h1>
+    </header>
+    <main>
+        <p>Aprete o botão para sortear um número entre <b>1</b> e <b>100</b></p>
+        <form action="index.php" method="post">
+            <input type="submit" value="Sortear">
+        </form>
+        <?php 
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $numero = rand(1, 100);
+                echo "<p>O número sorteado foi: <strong>$numero</strong></p>";
+            }
+        ?>
+    </main>
+</body>
+</html>
